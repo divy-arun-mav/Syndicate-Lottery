1c893d9cfaed8f95d39c9ffcf9cf3b02f9963ba1
Divy Mav <divymav5@gmail.com>
2025-03-08 13:50:52 +0530

    connecting wagmi provider with appkit

---
+++ syndicate/components/Web3/ConnectWallet.tsx
@@ -0,0 +1,10 @@
+"use client"
+
+import '@reown/appkit-wallet-button/react'
+
+const ConnectWallet = () => {
+    {/* @ts-expect-error msg */ }
+    return <appkit-button />
+}
+
+export default ConnectWallet;
